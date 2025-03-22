d8b9cebcd2938fc975ab12981545b5f5b62b3099
Shradha-2022 <91013212+shradha-2022@users.noreply.github.com>
2025-03-22 17:00:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    darkMode: "class", // Enables dark mode
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
